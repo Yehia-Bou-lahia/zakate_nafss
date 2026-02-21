6221bd22d01270f0cb3659b33e225cd82745c294
Yehia Boulahia <yehia.boulahia@protonmail.com>
2026-02-21 16:20:31 +0100

    feat(main): connect MainActivity to RamadanTheme and WelcomeScreen

--- app/src/main/java/com/example/ramadan/MainActivity.kt
+++ app/src/main/java/com/example/ramadan/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ramadan.ui.theme.RamadanTheme
+import com.example.ramadan.ui.theme.Screen.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RamadanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WelcomeScreen(
+                    //onStartClick = {/* navigate to another screen */}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RamadanTheme {
-        Greeting("Android")
     }
 }
