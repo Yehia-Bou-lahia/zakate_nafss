5383006f9cdeec4c5f6e0a1ee6eff55820ac1e21
Yehia Boulahia <yehia.boulahia@protonmail.com>
2026-02-27 12:28:51 +0100

    feat(main): connect MainActivity to WelcomeScreen

--- app/src/main/java/com/example/ramadan/MainActivity.kt
+++ app/src/main/java/com/example/ramadan/MainActivity.kt
@@ -4,15 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.ramadan.ui.screens.WelcomeScreen
 import com.example.ramadan.ui.theme.RamadanTheme
-import com.example.ramadan.ui.theme.Screen.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RamadanTheme {
                 WelcomeScreen(
-                    //onStartClick = {/* navigate to another screen */}
+                    onStartClick = {/* navigate to another screen */}
                 )
             }
         }
